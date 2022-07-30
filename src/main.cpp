ac81cab48672bd53e43764cce0931f2e174d2688
Agustín Del Pino <62936913+agustin-del-pino@users.noreply.github.com>
2022-07-30 22:38:39 +0000

    Create main entrypoint for simulate the arduino

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+// Arduino simulation setup
+void setup(){
+
+}
+
+// Arduino simulation loop
+void loop(){
+
+}
+
+
+// Entry point
+int isRunning = 1;
+
+int main(){
+    setup();
+
+    while(isRunning){
+        loop();
+    }
+
+    return 0;
+}
